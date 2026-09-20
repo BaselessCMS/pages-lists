4d6489cc4ddd9654b682b5f2cc2193624509998d
Controlled Chaos Design <greg@ccdzine.com>
2026-09-19 19:59:49 -0700

    Page description markup.

--- views/page-form.php
+++ views/page-form.php
@@ -30,9 +30,8 @@ $guide_page = DOMAIN_ADMIN . 'plugin/' . $this->className();
 	margin-top: 1rem;
 }
 </style>
-<div class="alert alert-primary alert-pages-lists" role="alert">
-	<p class="m-0"><?php $L->p( "Go to the <a href='{$guide_page}'>pages lists guide</a> page." ); ?></p>
-</div>
+
+<p class="page-description"><?php $L->p( "Go to the <a href='{$guide_page}'>pages lists guide</a> page." ); ?></p>
 
 <fieldset class="mt-4">
 	<legend class="screen-reader-text mb-3"><?php $L->p( 'Sidebar List Options' ) ?></legend>
